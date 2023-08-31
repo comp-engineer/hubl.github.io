be0625d75270de1f1a8816ed9fbfdb224b9de831
compengineer <chege.evanson@students.kyu.ac.ke>
2023-08-31 20:46:31 +0300

    Your commit message here

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Document(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128))
-    content = db.Column(db.Text)
-
-    def __repr__(self):
-        return f"<Document {self.title}>"
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/your_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
